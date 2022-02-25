5673b5e85b2a25fc8605d6810e2f5bd99049e9e6
Vico4 <victoire.stahl@gmail.com>
2022-02-25 21:45:13 +0100

    phrases completes

--- dreamBot.py
+++ dreamBot.py
@@ -19,12 +19,12 @@ client = tweepy.Client(
 
 # composition de la phrase
 base = "J'ai rêvé que je "
-verbes = ["mangeais", "tapais", "jouais", "rêvais", "buvais", "visais", "regardais", "fatiguais", "convoitais"]
+verbes = ["mangeais", "tapais", "jouais", "rêvais", "buvais", "visais", "regardais", "salissais", "convoitais"]
 complement = ["Paris","des grosses frites", "du code", "une chemise hawaienne", "les élections", "la vie", "le capitalisme", "les hommes", "la gloire", "le soleil"]
-#adjectif = ("magnifique", "")
-#complement2 = ()
+adjectif = ("parfaitement", "élégament", "en rythmes", "à l'arrache", "en mode one life", "brusquement", "violemment", "en noir et blanc", "sur du disco")
+complement2 = ("avec la Reine d'Angleterre", "avec François Fillon", "avec Michel Drucker", "tout en jouant au scrabble", "et ça passait à la télé", "et après les pompiers arrivaient", "et y avait des mariachis", "et je captais rien", "et en fait c'était une arnaque", "et je me suis réveillé", "sauf que j'étais de droite", "et j'achetais du poulet", "et après j'étais élue miss France")
 def makeTwitt():
-    tweet = base + random.choice(verbes) + " " + random.choice(complement)
+    tweet = base + random.choice(verbes) + " " + random.choice(complement) + " " + random.choice(adjectif) + " " + random.choice(complement2) + "."
     return tweet 
 
 #print(makeTwitt())
